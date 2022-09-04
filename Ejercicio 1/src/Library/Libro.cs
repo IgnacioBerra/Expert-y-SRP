c50f0895d8577cf2f945e09be307540611f3ecf1
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2022-09-04 00:43:42 -0300

    Ejercicio 1 terminado y con código comentado.

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -21,7 +21,9 @@ namespace SRP
         // El libro no deberia conocer información acerca de su libreria. 
         // Para solucionarlo, deberemos crear una class que se llame Libreria o similar, así si respetaría el principio SRP.
         // Esto se debe a que, con una clase llamada Libreria, esta se encargaría de conocer la ubicación del libro dentro de la libreria,
-        // Y no asume el libro esa responsabilidad, además de conocer la información del mismo. 
+        // y no asume el libro esa responsabilidad, además de conocer la información del mismo. 
+        // Cumpliendo con el principio Expert, para que la clase Libreria conozca el libro del cual esta guardando su ubicación en la biblioteca,
+        // se le pasa por parametro un objeto de la clase Book, es decir, un Libro en sí. 
         
 
     }
